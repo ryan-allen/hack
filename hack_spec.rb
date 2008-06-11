cfe2005c2d1b126e23cd3bbf96ea62bf38e65924
Ryan Allen <ryan@yeahnah.org>
2008-06-11 23:05:19 +1000

    specs should look... kinda like this, but gotta damn work

--- hack_spec.rb
+++ hack_spec.rb
@@ -1,7 +1,35 @@
 require 'hack'
 
 describe Hack do
-  it 'rules' do
-    true.should == true
+
+  def start_app!(&app)
+    @pid = fork do
+      require 'hack'
+      Hack.run!(:port => 4000, &app)
+    end
   end
+  
+  def stop_app!
+    `kill #{@pid}` if @pid
+    @pid = nil
+  def 
+  
+  before do  
+    start_app! do
+      get('/') { 'Hello World!' }
+    end
+  end
+  
+  after do
+    stop_app!
+  end
+  
+  def body_should_include(uri, string)
+    `curl http://0.0.0.0:4000#{uri}`.chomp.should =~ /#{string}/
+  end
+
+  it 'can map GET: /' do
+    body_should_include '/', 'Hello World!'
+  end
+  
 end
