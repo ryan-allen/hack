103d8a5a5175dfe562470751572ec8f708302ad3
Ryan Allen <ryan@yeahnah.org>
2008-11-02 23:13:22 +1100

    ah... forgot that web server spec thingo RFC whatzit says gotta be split on \r\n's... not sure that it was beer's fault, sure should ask him one day

--- spec/curl_helper.rb
+++ spec/curl_helper.rb
@@ -1,23 +1,27 @@
 module CurlHelper
   
   def curl(uri)
-    parse `curl http://0.0.0.0:5555#{uri} 2>/dev/null`.chomp
+    parse `curl -i http://0.0.0.0:5555#{uri} 2>/dev/null`.chomp
   end
 
 private
 
   def parse(output)
-    lines = output.split("\n")
+    lines = output.split(/\r\n|\n/)
     raw_status = lines.shift
     match = raw_status.match(/HTTP\/\d\.\d (\d+)/)
     status = match.captures[0].to_i
     boundary = lines.index('')
+    raise lines.inspect if boundary.nil?
     raw_headers, body = lines[0...boundary], lines[boundary+1..lines.length].join("\n")
     headers = {}
     raw_headers.each do |raw_header|
       headers[raw_header[0...raw_header.index(':')]] = raw_header[raw_header.index(':')+2..raw_header.length]
     end
     [status, headers, body]
+  rescue
+    puts output
+    raise $!
   end
 
 end
