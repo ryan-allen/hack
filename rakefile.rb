188df1bedeb40fff009d0854332e23c5b2371a56
Ryan Allen <ryan@ryan-allens-imac.local>
2009-01-06 18:07:47 +1100

    reformatting the readme as markdown coz i'm bored and installing my dev envinronment on a new 'pooter ;)

--- rakefile.rb
+++ rakefile.rb
@@ -3,3 +3,14 @@ task :default => :spec
 task :spec do
   puts `spec --color spec/hack_spec.rb`
 end
+
+task :markdown_to_html do
+  require 'rubygems'
+  require 'bluecloth'
+  `rm README.html`
+  open 'README.markdown', 'r' do |input|
+    open "README.html", 'w+' do |output|
+      output.write BlueCloth.new(input.read).to_html
+    end
+  end
+end
